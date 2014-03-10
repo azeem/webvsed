c22eb0d6f005057b528ece70fb676e8fa77b3d9b
azeem <kr00r4n@gmail.com>
2014-03-10 23:11:01 +0530

    cleanups

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,130 +1,51 @@
-/*global module:false*/
 module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
-
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: false,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-            "$": true,
-            "Webvs": true,
-            "SC": true,
-            "_": true,
-            "Dancer": true
-        }
-      },
-      files: ['Gruntfile.js', "app/**/*.js"]
-    },
-
-    copy: {
-        dev: {
-            files: [
-                {expand: true, flatten:false, cwd: "app/", src: ["./**/*.!(html)"], dest: "build/"},
-            ]
+    grunt.initConfig({
+        jshint: {
+            files: ['Gruntfile.js', "js/**/*.js"]
         },
-        dist: {
-            files: [
-                {dest:"dist/bootstrap",  cwd:"bower_components/bootstrap/bootstrap", src:"**/*",             expand:true, flatten:false},
-                {dest:"dist/jquery-ui",  cwd:"bower_components/jquery-ui",           src:"themes/base/**/*", expand:true, flatten:false},
-                {dest:"dist/css/jqtree.css",     src:"bower_components/jqtree/jqtree.css"},
-                {dest:"dist/css/spectrum.css",   src:"bower_components/spectrum/spectrum.css"},
-                {expand: true, flatten:false, cwd: "app/", src: ["./**/*.!(html|js)"], dest: "dist/"},
-            ]
-        },
-    },
 
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        files: {
-            "dist/js/main.min.js": "app/js/editor.js",
-            "dist/js/libs.min.js": [
-                "bower_components/jquery/jquery.js",
-                "bower_components/jquery-ui/ui/jquery-ui.js",
-                "bower_components/underscore/underscore.js",
-                "bower_components/dancer.js/lib/fft.js",
-                "bower_components/dancer.js/dancer.js",
-                "bower_components/stats.js/build/stats.min.js",
-                "bower_components/spectrum/spectrum.js",
-                "bower_components/jsonform/lib/jsonform.js",
-                "bower_components/jqtree/tree.jquery.js",
-                "bower_components/bootstrap/bootstrap/js/bootstrap.js",
-                "bower_components/webvs/lib/D.min.js",
-                "bower_components/webvs/dist/webvs.min.js"
-            ]
-        }
-      }
-    },
-
-    preprocess: {
-        dev: {
-            options: {
-                context: {
-                    mode: "dev"
+        uglify: {
+            dist: {
+                files: {
+                "dist/js/main.min.js": "js/**/*.js",
                 }
-            },
-            files: {
-                "build/index.html": "app/index.html",
             }
         },
-        dist: {
-            options: {
-                context: {
-                    mode: "dist"
+
+        clean: {
+            dist: ['dist/*']
+        },
+
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: ".",
+                    directory: ".",
+                    livereload: true
                 }
-            },
-            files: {
-                "dist/index.html": "app/index.html",
+            }
+        },
+
+        watch: {
+            app: {
+                files: ["js/**/*.js", "css/**/*.css", "index.html"],
+                tasks: ["jshint"],
+                options: {
+                    livereload: true
+                }
             }
         }
-    },
+    });
 
-    clean: {
-      dev: ['build/*'],
-      dist: ['dist/*'],
-    }
-    /*watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-    }*/
-  });
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks("grunt-contrib-connect");
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks("grunt-preprocess");
-
-  // Default task.
-  grunt.registerTask('default', ["clean:dev", 'jshint', 'copy:dev', 'preprocess:dev']);
-  grunt.registerTask('dist', ["clean:dist", 'jshint', 'copy:dist', 'uglify', 'preprocess:dist']);
-
+    // Default task.
+    grunt.registerTask('default', ['clean:dist', 'jshint', 'uglify:dist']);
+    grunt.registerTask("debug", ["watch"]);
 };
